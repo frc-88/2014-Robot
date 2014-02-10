886d3540b96c7cd5e047b13fa5394c12ef430c26
Nick Avtges <>
2014-02-10 08:11:39 -0500

    A bit more cleanup

--- src/edu/wpi/first/wpilibj/templates/JagPair.java
+++ src/edu/wpi/first/wpilibj/templates/JagPair.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package edu.wpi.first.wpilibj.templates;
 
 import edu.wpi.first.wpilibj.CANJaguar;
@@ -14,63 +13,61 @@ import edu.wpi.first.wpilibj.PIDSource;
 import edu.wpi.first.wpilibj.can.CANTimeoutException;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-
 /**
- *
- * @author David
+ * @author TJ^2 Programming Team
  */
 public class JagPair implements PIDOutput {
 
-    byte jagGroup = 1;
-    private CANJaguar jag1, jag2;
-    private Encoder encoder;
-    private PIDController controller;
     private static final double p = .05;
     private static final double i = 0.0;
     private static final double d = 0.0;
     private static final double f = 0.2;
     private static final double cycleTime = .020;
+    private static byte jagGroup = 1;
     private static final int DRIVE_ENCODER_LINES = 250;
     //feet per revolution over encoder lines
-    private static final double DISTANCE_PER_REVOLUTION = 1.57/DRIVE_ENCODER_LINES;
+    private static final double DISTANCE_PER_REVOLUTION = 1.57 / DRIVE_ENCODER_LINES;
+
+    private CANJaguar jag1, jag2;
+    private Encoder encoder;
+    private PIDController controller;
+    // private double last_speedL = 0.0;
+    // private double last_speedR = 0.0;
     private boolean m_closedLoop;
     private boolean m_fault;
-    
+
     public JagPair(int jag1In, int jag2In, int encoderA, int encoderB) {
         try {
-                jag1 = new CANJaguar(jag1In);
-                jag2 = new CANJaguar(jag2In);
-                
+            jag1 = new CANJaguar(jag1In);
+            jag2 = new CANJaguar(jag2In);
+
 //                if (jag1 != null) {
 //                    jag1.configNeutralMode(CANJaguar.NeutralMode.kCoast);
 //                    jag2.configNeutralMode(CANJaguar.NeutralMode.kCoast);
 //                }
-                
-            }
-            catch (CANTimeoutException ex) {
-                SmartDashboard.putBoolean("CAN ERROR drive ", m_fault);
-                m_fault = true;
-            }
-               
-        encoder =  new Encoder(encoderA,encoderB);
+        } catch (CANTimeoutException ex) {
+            SmartDashboard.putBoolean("CAN ERROR drive ", m_fault);
+            m_fault = true;
+        }
+
+        encoder = new Encoder(encoderA, encoderB);
         encoder.setDistancePerPulse(DISTANCE_PER_REVOLUTION);
         encoder.setSamplesToAverage(6);
         encoder.setPIDSourceParameter(PIDSource.PIDSourceParameter.kRate);
         encoder.reset();
         encoder.start();
-        
-        controller = new PIDController(p,i,d,f,encoder,this, cycleTime);
-        controller.enable();
 
+        controller = new PIDController(p, i, d, f, encoder, this, cycleTime);
+        controller.enable();
     }
-        /**
+
+    /**
      * Enables ClosedLoop control Driving. It sets it to speed.
      */
     public void enableClosedLoop() {
-        double position;
         System.out.println("Enabling closed loop control");
-        
-        if(jag1 != null && jag2 != null) {
+
+        if (jag1 != null && jag2 != null) {
             try {
                 // set the motors to closed loop
                 //may be percent v bus
@@ -86,14 +83,12 @@ public class JagPair implements PIDOutput {
             }
         }
     }
-    
-    
-    
-         /**
+
+    /**
      * Disables the Drive closed loop and puts it into open loop.
      */
     public void disableClosedLoop() {
-        if(jag1 != null && jag2 != null) {
+        if (jag1 != null && jag2 != null) {
             try {
                 // set the motors to open loop
                 jag1.changeControlMode(CANJaguar.ControlMode.kPercentVbus);
@@ -108,14 +103,15 @@ public class JagPair implements PIDOutput {
         }
     }
 
-        /**
-     * Returns whether or not jagPair is in ClosedLoop. If it is it will return true and if it is not
-     * it will return false.
+    /**
+     * Returns whether or not jagPair is in ClosedLoop. If it is it will return
+     * true and if it is not it will return false.
      */
     public boolean isClosedLoop() {
         return m_closedLoop;
     }
-        /**
+
+    /**
      * Returns the value of the fault flag
      */
     public boolean getFault() {
@@ -125,15 +121,14 @@ public class JagPair implements PIDOutput {
     public void setX(double x) {
         if (jag1 != null || jag2 != null) {
             try {
-                jag1.setX(x,jagGroup);
-                jag2.setX(x,jagGroup);
+                jag1.setX(x, jagGroup);
+                jag2.setX(x, jagGroup);
                 CANJaguar.updateSyncGroup(jagGroup);
             } catch (CANTimeoutException ex) {
                 m_fault = true;
                 SmartDashboard.putBoolean("CAN ERROR drive", m_fault);
             }
         }
-
     }
 
     /**
@@ -146,7 +141,7 @@ public class JagPair implements PIDOutput {
         double maxSpeedHighGear = 12.4; // feet per second
         double maxSpeedLowGear = 5.2;   // feet per second
         double maxSpeed = 0;
-       
+
         if (isHighGear) {
             maxSpeed = maxSpeedHighGear;
         } else {
@@ -174,9 +169,8 @@ public class JagPair implements PIDOutput {
 //        }
 //        last_speedL = lSpeed;
 //        last_speedR = rSpeed;
-
     }
-    
+
     public void updateSmartDashboard(String name) {
         SmartDashboard.putNumber(name + " speed actual ", encoder.getRate());
 
@@ -190,38 +184,36 @@ public class JagPair implements PIDOutput {
 
         }
     }
-        /**
-     * @returns speed of wheels 
+
+    /**
+     * @returns speed of wheels
      */
     public double getSpeed() {
         return encoder.getRate();
     }
 
-    public double getDistance(){
+    public double getDistance() {
         return encoder.getDistance();
-    } 
-    
-     /**
-    * Reset the encoders to 0
-    */
+    }
+
+    /**
+     * Reset the encoders to 0
+     */
     public void resetDistance() {
         encoder.reset();
-           }
-    
-     public void enablePID(){
+    }
+
+    public void enablePID() {
         controller.enable();
-           }
-     
-      public void disablePID() {
+    }
+
+    public void disablePID() {
         controller.disable();
-            }
-     
-    
-    
+    }
+
     public void pidWrite(double output) {
         jag1.pidWrite(output);
         jag2.pidWrite(output);
-        
+
     }
-    
 }
