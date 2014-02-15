3d23843df1bfca3e2d2e71af7f4d39119048c3ad
smartas <smartas@10.0.10.193>
2014-02-15 14:24:11 -0500

    Clean Arms, Kicker, Rollers (mostly removing old CAN references)

--- src/edu/wpi/first/wpilibj/templates/subsystems/Kicker.java
+++ src/edu/wpi/first/wpilibj/templates/subsystems/Kicker.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.wpi.first.wpilibj.templates.subsystems;
 
-//import edu.wpi.first.wpilibj.CANJaguar;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Jaguar;
-//import edu.wpi.first.wpilibj.can.CANTimeoutException;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.templates.Wiring;
@@ -18,38 +11,26 @@ import edu.wpi.first.wpilibj.templates.Wiring;
  * @author David
  */
 public class Kicker extends Subsystem {
-    
+
     //Have to wait as we may be changing what we use
     DigitalInput lightSensor1;
     Jaguar kickerJag;
-    
+
     public Kicker() {
         lightSensor1 = new DigitalInput(Wiring.lightSensor);
-        
-        //try {
-      kickerJag = new Jaguar(Wiring.kickerJag);
-        //}catch(CANTimeoutException ex) {
-                //SmartDashboard.putBoolean("CAN ERROR choo choo", true);
-        //}
+        kickerJag = new Jaguar(Wiring.kickerJag);
     }
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
-    
+
     public boolean ReturnLightSensorValue() {
-        SmartDashboard.putBoolean("light sensor choo choo ", lightSensor1.get()); 
+        SmartDashboard.putBoolean("light sensor choo choo ", lightSensor1.get());
         return lightSensor1.get();
     }
-    
-    public void KickerOpenLoop(double value){
-        //try{
-            kickerJag.set(value);
-        //}catch(CANTimeoutException ex){
-            SmartDashboard.putDouble("kicker power", value);
-        //}
+
+    public void KickerOpenLoop(double value) {
+        kickerJag.set(value);
+        SmartDashboard.putDouble("kicker power", value);
     }
 
     public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
     }
 }
